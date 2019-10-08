c24b531bb4e297071284ef146dac466350da32d6
Amjed Ayoub <amjadkayoub@gmail.com>
2019-10-07 19:04:37 -0700

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,291 @@
+var friends = [{
+        "name": "Ahmed",
+        "photo": "https://tse3.mm.bing.net/th?id=OIP.vx2aJjjU0UbRstTND3hjegHaGK&pid=Api&P=0&w=213&h=178",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.34L2LSY75Zjb5TMHXaB8BAHaI7&pid=Api&P=0&w=300&h=300",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://images.search.yahoo.com/images/view;_ylt=Awr9DWsP7JtdilsAbHiJzbkF;_ylu=X3oDMTI0cjNhdHZ0BHNlYwNzcgRzbGsDaW1nBG9pZAM2ZGRkNmE0ZTExOTRhMWU0NmU0MDRkMDQ4NGNlMzhlMgRncG9zAzQ2OQRpdANiaW5n?back=https%3A%2F%2Fimages.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Davatar%2Bcharacters%26n%3D60%26ei%3DUTF-8%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26nost%3D1%26tab%3Dorganic%26ri%3D469&w=593&h=517&imgurl=i.pinimg.com%2Foriginals%2Ff5%2F7e%2Fda%2Ff57edafd966f88ece64e465c42d0161f.jpg&rurl=https%3A%2F%2Fwww.pinterest.com.au%2Fpin%2F593278950886695101%2F&size=15.3KB&name=My+roblox+character+Add+me...wateryminecraftboy22+%7C+Papers+%7C+Cool+avatars%2C+Roblox+animation+...&p=avatar+characters&oid=6ddd6a4e1194a1e46e404d0484ce38e2&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=My+roblox+character+Add+me...wateryminecraftboy22+%7C+Papers+%7C+Cool+avatars%2C+Roblox+animation+...&b=421&ni=320&no=469&ts=&tab=organic&sigr=11kqh6u6t&sigb=14i6i7fkp&sigi=124e5usu7&sigt=12vhh0vjb&sign=12vhh0vjb&.crumb=iXVWhmIf88K&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Sarah",
+        "photo": "https://i.pinimg.com/originals/5a/70/de/5a70de7c77b12a72004c09400fb42daa.png",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "2"
+        ]
+    },
+    {
+        "name": "Emily",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.SuYujfPPPt7-qqI2e0m2HwHaKh&pid=Api&P=0&w=300&h=300",
+        "scores": [
+            "5",
+            "4",
+            "2",
+            "1",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Rogelio R Zavala",
+        "photo": "https://tse4.mm.bing.net/th?id=OIP.mo7XYA0Am2bh0NHxsGk1bAHaFj&pid=Api&P=0&w=208&h=157",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "3",
+            "2",
+            "3",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Lola",
+        "photo": "https://c8.alamy.com/comp/MND3G3/vector-character-design-concept-superhero-woman-in-superwoman-pose-in-blue-and-red-costume-with-red-cape-and-purple-hair-MND3G3.jpg",
+        "scores": [
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "2",
+            "4",
+            "3",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Bobby Lightfoot",
+        "photo": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/11/07/12/bigfoot-0.jpg?w968",
+        "scores": [
+            "4",
+            "2",
+            "4",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Hi ",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Lili",
+        "photo": "https://c8.alamy.com/comp/MND3G3/vector-character-design-concept-superhero-woman-in-superwoman-pose-in-blue-and-red-costume-with-red-cape-and-purple-hair-MND3G3.jpg",
+        "scores": [
+            "3",
+            "1",
+            "2",
+            "1",
+            "4",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "me",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.r8_kUpejOC_RWdveXIv8dQHaKW&pid=Api&P=0&w=300&h=300",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "giphyapp",
+        "photo": "https://tse1.mm.bing.net/th?id=OIP.fEstyBvbrWiYhLRPDqv55wHaHa&pid=Api&P=0&w=300&h=300",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "5",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Timmy",
+        "photo": "https://southparkstudios.mtvnimages.com/images/shows/south-park/episode-thumbnails/season-4/south-park-s04e04-timmy-2000_4x3.jpg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "5",
+            "2",
+            "3",
+            "4",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "todo",
+        "photo": "http://mediamedusa.com/wp-content/uploads/2013/04/avatar.jpg",
+        "scores": [
+            "1",
+            "2",
+            "2",
+            "1",
+            "3",
+            "3",
+            "2",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Patrick",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuRcYdvAf91LJVGqI_UANXErhA8qhUit4lSAZ451g0V5eP3Jrr",
+        "scores": [
+            "1",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5",
+            "1",
+            "1",
+            "5",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
